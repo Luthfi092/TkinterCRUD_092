d973464de16f134f5fe517d244df82a231f2b4ba
upi1682 <upi538287@gmail.com>
2025-11-27 14:35:01 +0700

    Membuat tablel nama dan pelajaran

--- p9.py
+++ p9.py
@@ -6,7 +6,21 @@ import csv
 
 DB_FILE = 'nilai_siswa.db'
 
-
+def init_db():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nilai_siswa (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama_siswa TEXT NOT NULL,
+            biologi REAL NOT NULL,
+            fisika REAL NOT NULL,
+            inggris REAL NOT NULL,
+            prediksi_fakultas TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 
 
